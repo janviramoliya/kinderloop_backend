6c45b28a3b0f862cb857a0556af44217d1bea97e
janviramoliya <janviramoliya27@gmail.com>
2025-11-26 23:44:35 +0530

    Add setup files
    Add setup files

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint', 'import'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'plugin:import/typescript',
+    'prettier'
+  ],
+  rules: {
+    'import/order': [
+      'warn',
+      {
+        groups: [['builtin', 'external'], 'internal', ['parent', 'sibling', 'index']],
+        'newlines-between': 'always'
+      }
+    ]
+  },
+  ignorePatterns: ['dist', 'node_modules']
+};
